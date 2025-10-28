4ca7b69b69eda57381d23e8465b7fb7b7b0ffcc0
michi <bergen.michael@web.de>
2025-10-28 15:55:13 +0100

    ende

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 namespace ArrayListe
 {
     internal class Program
@@ -7,39 +8,48 @@ namespace ArrayListe
         static void Main(string[] args)
         {
             Person[] pers = new Person[4];
-            pers[0] = new Person() { Name = "[0] Anton" };
-            pers[1] = new Person() { Name = "[1] Mehmet"};
-            pers[2] = new Person() { Name = "[2] Aladin" };
-            pers[3] = new Person() { Name = "[3] Frank" };
+            pers[0] = new Person() { Name = "[0] Anton" , City = "München" };
+            pers[1] = new Person() { Name = "[1] Mehmet" , City = "Berlin"};
+            pers[2] = new Person() { Name = "[2] Aladin" , City = "Bremen" };
+            pers[3] = new Person() { Name = "[3] Frank" , City = "Frankfurt" };
+            ArrayList arrlist1 = new ArrayList();
+            
+        
+            
+            
           
+
+
             ArrayList arrlist = new ArrayList();
-            arrlist.AddRange(pers);
+        
             List<Person> pliste = new List<Person>();
+            
+           
+
             pliste.AddRange(pers);
             Console.WriteLine("PListe enthält :\n{0}", pliste.Count);
-            Console.WriteLine("HashCode der Pliste ist:\n{0}", pliste.GetHashCode());
+            Console.WriteLine("HashCode der Pliste ist:\n{0}\n", pliste.GetHashCode());
             Console.WriteLine("ArrayList kann {0} Personen aufnehmen. ", arrlist.Capacity);
-            Console.WriteLine("ArrayList enthält {0} Personen", arrlist.Count);
+            Console.WriteLine("ArrayList enthält {0} Personen\n", arrlist.Count);
             foreach (Person p in arrlist)
             {
                 Console.WriteLine(p.Name + " " + p.City);
             }
             arrlist.Reverse();
-            Console.WriteLine("HashCode der ArrayListe \n :{0}", arrlist.GetHashCode());
-            Console.WriteLine("Nach dem Reverse:\n");
+            Console.WriteLine("HashCode der ArrayListe: \n {0}\n", arrlist.GetHashCode());
+            Console.WriteLine("Nach dem Reverse ");
             foreach (Person p in arrlist)
             {
                 Console.WriteLine(p.Name + " " + p.City);
             }
-            Console.WriteLine("Jetzt wird sortiert und nochmal ausgegeben:");
-            arrlist.Sort();
-            Console.WriteLine(pliste.sort()
-            
-
+            arrlist.Clear();
+            Console.WriteLine("ArrList wurde gecleart enthält jetzt Elemente:  {0} ", arrlist.Count);
+            Console.WriteLine("ArrList reserviert den Speicher für Elemente: " + arrlist.Capacity );
+        
         }
         internal class Person {
-            public string Name { get; set; }
-            public string City { get; set; }
+            internal string Name { get; set; }
+            internal string City { get; set; }
         }
     }
 }
